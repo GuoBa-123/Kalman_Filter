c4d2fca4a8942052c4e047a0f4c13a7c45a6825e
8-b1t <guoba114514@163.com>
2026-03-03 17:22:16 +0800

    去掉了.cc文件 声明保留在.h里

--- KalmanFilter.h
+++ KalmanFilter.h
@@ -6,9 +6,12 @@ class Kalman_Filter{
 public:
 math::matrix<N,1>z;//观测值
 math::matrix<M,1>kf_out;//输出值
-void Kalman_init(math::matrix<M,1>X0, math::matrix<M,M> A_, math::matrix<M,M> P0, math::matrix<M,M> Q_, math::matrix<N,N> R_, math::matrix<N,M> H_){
+
+bool skip_fun[5]= {false};//是否跳过KF的五式中的某一式，默认不跳过
+
+void Kalman_init(math::matrix<M,1>X0, math::matrix<M,M> F_, math::matrix<M,M> P0, math::matrix<M,M> Q_, math::matrix<N,N> R_, math::matrix<N,M> H_){
     this->X = X0;
-    this->A = A_;
+    this->F = F_;
     this->P = P0;
     this->Q = Q_;
     this->R = R_;
@@ -17,22 +20,53 @@ void Kalman_init(math::matrix<M,1>X0, math::matrix<M,M> A_, math::matrix<M,M> P0
 
 void Kalman_update(math::matrix<N,1> z){
     // 预测
-    X = A * X;
-    P = A * P * A.transpose() + Q;
+    state_predict();
+    P_predict();
 
     //更新
-    K = P * H.transpose() * (H * P * H.transpose() + R).inv();
-    X = X + K * (z - H * X);
-    P = (math::matrix<M,M>(1) - K * H) * P;
+    K_caculate();
+    state_update(z);
+    P_update();
+
+    //输出
     kf_out = X;
 }
 
 private:
-math::matrix<M,M>A;//状态转移矩阵
+math::matrix<M,M>F;//状态转移矩阵
+void(*F_func)(math::matrix<M,1>X);//状态转移函数
 math::matrix<M,1>K;//卡尔曼增益矩阵
 math::matrix<M,M>P;//误差协方差矩阵
 math::matrix<M,1>X;//初始矩阵
 math::matrix<M,M>Q;//过程噪声协方差矩阵
 math::matrix<N,N>R;//测量噪声协方差矩阵
 math::matrix<N,M>H;//状态观测矩阵
+
+//KF的黄金五式
+/*1.状态预测*/
+void state_predict(){
+    if(!skip_fun[0])
+    X = F * X;
+}
+/*2.协方差预测*/
+void P_predict(){
+    if(!skip_fun[1])
+     P = F * P * F.transpose() + Q;
+}
+/*3.卡尔曼增益计算*/
+void K_caculate(){
+    if(!skip_fun[2])
+    K = P * H.transpose() * (H * P * H.transpose() + R).inv();
+}
+/*4.数据融合*/
+void state_update(math::matrix<N,1> z){
+    if(!skip_fun[3])
+    X = X + K * (z - H * X);
+}
+/*5.更新误差协方差矩阵*/
+void P_update(){
+    if(!skip_fun[4])
+    P = (math::matrix<M,M>(1) - K * H) * P;
+}
+
 };
--- main.cc
+++ main.cc
@@ -5,21 +5,15 @@ using namespace std;
 int main() {
     Kalman_Filter<2,1> kf;
     //kalman init
+float dt = 2.0f;
     math::matrix<2,1> X0 = {0,0};
-    math::matrix<2,2> P0 = {1,0,0,1};
-    math::matrix<2,2> Q;
-    Q(0,0) = 0.01;
-    Q(1,1) = 0.01;
-    Q(0,1) = 0;
-    Q(1,0) = 0;
-
-    math::matrix<1,1> R;
-    R(0,0) = 0.1;
-
+    math::matrix<2,2> P0({100,0,0,100});
+    math::matrix<2,2> Q({0.01f,0.f,0.f,0.01f});
+    math::matrix<1,1> R({0.1f});
     math::matrix<1,2> H = {1,0};
-    math::matrix<2,2>A = {1,1,0,1};
+    math::matrix<2,2> F({1.0f,dt,0.0f,1.0f});
     
-    kf.Kalman_init(X0, A, P0, Q, R, H);
+    kf.Kalman_init(X0, F, P0, Q, R, H);
        float obversation[10] = {0.0, 0.9, 2.1, 3.05, 3.95, 5.02, 5.98, 7.03, 7.97, 9.01};
     for(int i=0;i<10;i++){
         kf.Kalman_update(math::matrix<1,1>(obversation[i]));
